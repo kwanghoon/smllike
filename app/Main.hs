ce9dc4f5d1054603e7a4b4774469840dfa6196e9
Kwanghoon Choi <lazyswamp@gmail.com>
2020-07-12 12:04:54 +0900

    Remove the parser part from Main

--- app/Main.hs
+++ app/Main.hs
@@ -2,21 +2,16 @@ module Main where
 
 import CommonParserUtil
 
-import Lexer
-import Terminal
-import Parser
-import Expr
-
-import System.IO
-
-------------------------
--- The syntax completion
-------------------------
-
 import Token
+import Expr
+import Lexer
+import Parser
+
 import EmacsServer
 import SynCompInterface
 import Control.Exception
+
+import System.IO
 
 main :: IO ()
 main = do
@@ -30,39 +25,4 @@ computeCand programTextUptoCursor isSimpleMode = ((do
   `catch` \e -> case e :: LexError of _ -> handleLexError
   `catch` \e -> case e :: ParseError Token AST of _ -> handleParseError isSimpleMode e)
   
---------------------
--- The normal parser
---------------------
-
--- main :: IO ()
--- main = do
---   fileName <- readline "Enter your file: "
---   case fileName of
---     "exit" -> return ()
---     line -> doProcess line
-
-doProcess line = do
-  putStrLn ("Opening " ++ line)
-  text <- readFile line
-  putStrLn "Lexing..."
-  terminalList <- lexing lexerSpec text
-  putStrLn "Parsing..."
-  exprSeqAst <- parsing parserSpec terminalList
-  putStrLn "Pretty Printing..."
-  putStrLn (show exprSeqAst)
-  
-  
-readline msg = do
-  putStr msg
-  hFlush stdout
-  readline'
-
-readline' = do
-  ch <- getChar
-  if ch == '\n' then
-    return ""
-  else
-    do line <- readline'
-       return (ch:line)
-
 
