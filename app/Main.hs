b8ee601c15a3366360afb2fdc7fe766abf380d95
Kwanghoon Choi <lazyswamp@gmail.com>
2021-10-28 14:59:53 +0100

    upgrade yapb version

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import CommonParserUtil
 
+import TokenInterface
 import Token
 import Expr
 import Lexer
@@ -10,19 +11,65 @@ import Parser
 import EmacsServer
 import SynCompInterface
 import Control.Exception
+import Data.Typeable
 
 import System.IO
 
 main :: IO ()
 main = do
-  emacsServer computeCand
+  emacsServer (computeCand False)
 
-computeCand :: String -> Bool -> IO [EmacsDataItem]
-computeCand programTextUptoCursor isSimpleMode = ((do
-  terminalList <- lexing lexerSpec programTextUptoCursor 
-  ast <- parsing parserSpec terminalList 
-  successfullyParsed)
-  `catch` \e -> case e :: LexError of _ -> handleLexError)
-  `catch` \e -> case e :: ParseError Token AST of _ -> handleParseError isSimpleMode e
+maxLevel = 10000
+
+data ParseErrorWithLineCol token ast = ParseErrorWithLineCol Int Int (ParseError token ast)
+  deriving (Typeable, Show)
+
+instance (TokenInterface token, Typeable token, Show token, Typeable ast, Show ast)
+  => Exception (ParseErrorWithLineCol token ast)
+
+computeCand :: Bool -> String -> String -> Bool -> IO [EmacsDataItem]
+computeCand debug programTextUptoCursor programTextAfterCursor isSimpleMode = do
+  ((computeCand_ isSimpleMode programTextUptoCursor programTextAfterCursor
+    `catch` \e -> case e :: LexError of _ -> handleLexError)
+    `catch` \e -> case e :: ParseErrorWithLineCol Token AST of ParseErrorWithLineCol line column e -> do {
+        (_, _, terminalListAfterCursor) <- lexingWithLineColumn lexerSpec line column programTextAfterCursor;
+        handleParseError (
+          HandleParseError {
+              debugFlag=debug,
+              searchMaxLevel=maxLevel,
+              simpleOrNested=isSimpleMode,
+              postTerminalList=terminalListAfterCursor,
+              nonterminalToStringMaybe=Nothing}) e
+        })
+    
+computeCand_ :: Bool -> String -> String -> IO [EmacsDataItem]
+computeCand_ isSimpleMode programTextUptoCursor programTextAfterCursor = do
+  (line, column, terminalListUptoCursor)  <- lexingWithLineColumn lexerSpec 1 1 programTextUptoCursor
+  
+  ast <-
+    (parsing False parserSpec terminalListUptoCursor
+      `catch` \e -> case e :: ParseError Token AST of  _ -> throw (ParseErrorWithLineCol line column e))
+
+  successfullyParsed
+
+-- computeCand :: Bool -> String -> String -> Bool -> IO [EmacsDataItem]
+-- computeCand debug programTextUptoCursor programTextAfterCursor isSimpleMode = ((do
+--   (line, column, terminalList) <- lexingWithLineColumn lexerSpec 1 1 programTextUptoCursor 
+--   ast <- parsing False parserSpec terminalList 
+--   successfullyParsed)
+--   `catch` \e -> case e :: LexError of _ -> handleLexError)
+--   `catch` \e ->
+--      case e :: ParseErrorWithLineCol Token AST of
+--        ParseErrorWithLineCol line column e ->
+--           do (_,_,terminalListAfterCursor) <-
+--                 lexingWithLineColumn lexerSpec line column programTextAfterCursor
+            
+--              handleParseError (
+--                HandleParseError {
+--                    debugFlag=debug,
+--                    searchMaxLevel=maxLevel,
+--                    simpleOrNested=isSimpleMode,
+--                    postTerminalList=terminalListAfterCursor,
+--                    nonterminalToStringMaybe=Nothing}) e
   
 
